486e31fe68a070f4ae24262c3b714c87fd69eddf
Rayanoz <444101417@student.ksu.edu.sa>
2025-04-11 22:26:59 +0300

    Implemenet classes

--- app/src/main/java/PhotoApp/Album.java
+++ app/src/main/java/PhotoApp/Album.java
@@ -5,18 +5,12 @@ public class Album {
     private String condition;
     private PhotoManager manager;
     private int nbComps;
-    private LinkedList<Photo> photos;
-    private LinkedList<Photo> allPhotos;
-    private LinkedList<Photo> result;
 
     public Album(String name, String condition, PhotoManager manager) {
         this.name = name;
         this.condition = condition;
         this.manager = manager;
-        this.allPhotos = manager.getPhotos();
-        this.result = new LinkedList<Photo>();
         this.nbComps = 0;
-        this.photos = new LinkedList<Photo>();
     }
 
     public String getName() {
@@ -31,34 +25,62 @@ public class Album {
         return manager;
     }
 
-    public LinkedList<Photo> getPhotos() {
-        if (!photos.empty()) {
-            return result;
-        }
-        allPhotos.findFirst();
-        while (!allPhotos.last()) {
-            Photo currentPhoto = allPhotos.retrieve();
-            if (currentPhoto.getTags().contains(condition)) {
-                result.insert(currentPhoto);
-            }
-            allPhotos.findNext();
-        }
-        return result;
-    }
-
     public int getNbComps() {
-        if (!photos.empty()) {
-            return nbComps;
-        }
-        allPhotos.findFirst();
-        while (!allPhotos.last()) {
-            Photo currentPhoto = allPhotos.retrieve();
-            if (currentPhoto.getTags().contains(condition)) {
-                result.insert(currentPhoto);
-            }
-            nbComps++;
-            allPhotos.findNext();
-        }
         return nbComps;
     }
+
+    public LinkedList<Photo> getPhotos() {
+        // get rif of the condition"AND" and get the photos of the album.
+        String[] tags = condition.split(" AND ");
+        for (int i = 0; i < tags.length; i++) {
+            tags[i] = tags[i].trim();
+        }
+        // iterate in every photo and check if the tags of the photo are a subset of the
+        // tags.
+        // if it is, add the photo to the album.
+        LinkedList<Photo> photos = new LinkedList<Photo>();
+        manager.getPhotos().findFirst();
+        while (!manager.getPhotos().last()) {
+            Photo photo = manager.getPhotos().retrieve();
+            if (photo != null) {
+                if (subset(photo.getTags(), tags)) {
+                    photos.insert(photo);
+                }
+            }
+            manager.getPhotos().findNext();
+        }
+        if (manager.getPhotos().retrieve() != null) {
+            Photo photo = manager.getPhotos().retrieve();
+            if (subset(photo.getTags(), tags)) {
+                photos.insert(photo);
+            }
+        }
+        return photos;
+
+    }
+
+    public boolean tagContain(LinkedList<String> tags, String tag) {
+        tags.findFirst();
+        while (!tags.last()) {
+            if (tags.retrieve().equals(tag))
+                return true;
+            tags.findNext();
+        }
+        if (tags.retrieve().equals(tag))
+            return true;
+        return false;
+    }
+
+    // this method is used to check if the tags of a photo are a subset of the tags
+    // of an album.
+    // it returns true if the tags of the photo are a subset of the tags of the
+    // album, false otherwise.
+    public boolean subset(LinkedList<String> tags, String[] tag) {
+        for (int i = 0; i < tag.length; i++) {
+            if (!tagContain(tags, tag[i]))
+                return false;
+        }
+        return true;
+    }
+
 }
