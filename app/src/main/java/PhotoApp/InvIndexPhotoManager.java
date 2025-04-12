dbd9cdb9d58c971238143a19d8caea8aafe38133
NaifAlabdulkareem <nayefalabdulareem@gmail.com>
2025-04-12 19:09:21 +0300

    inv index class done, need to be reviewed

--- app/src/main/java/PhotoApp/InvIndexPhotoManager.java
+++ app/src/main/java/PhotoApp/InvIndexPhotoManager.java
@@ -36,9 +36,61 @@ public void addPhoto(Photo photo) {
 }
 
 
-    public void deletePhoto(String path){
-        
+    public void deletePhoto(String path) {
+    if (path == null || invertedIndex.empty()) {
+        return;
     }
+
+    // Store tags to be removed (tags with no photos after deletion)
+    LinkedList<String> tagsToRemove = new LinkedList<>();
+    
+    // Traverse the BST using a recursive helper method
+    deletePhotoHelper(path, invertedIndex.root, tagsToRemove);
+    
+    // Remove tags that have no photos
+    tagsToRemove.findFirst();
+    while (!tagsToRemove.empty()) {
+        String tagToRemove = tagsToRemove.retrieve();
+        invertedIndex.remove_key(tagToRemove);
+        if (tagsToRemove.last()) {
+            break;
+        }
+        tagsToRemove.findNext();
+    }
+}
+
+private void deletePhotoHelper(String path, BST<LinkedList<Photo>>.BSTNode<LinkedList<Photo>> node, LinkedList<String> tagsToRemove) {
+    if (node == null) {
+        return;
+    }
+
+    // Process current node
+    LinkedList<Photo> photos = node.data;
+    photos.findFirst();
+    boolean photoFound = false;
+    
+    while (true) {
+        Photo currentPhoto = photos.retrieve();
+        if (currentPhoto.getPath().equals(path)) {
+            photos.remove();
+            photoFound = true;
+            break;
+        }
+        if (photos.last()) {
+            break;
+        }
+        photos.findNext();
+    }
+
+    // If the photos list is empty after removal, add tag to removal list
+    if (photoFound && photos.empty()) {
+        tagsToRemove.insert(node.key);
+    }
+
+    // Recursively process left and right subtrees
+    deletePhotoHelper(path, node.left, tagsToRemove);
+    deletePhotoHelper(path, node.right, tagsToRemove);
+}
     public BST<LinkedList<Photo>> getPhotos(){
         return invertedIndex;
     }
